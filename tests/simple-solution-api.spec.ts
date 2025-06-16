d44f69e6737688a78686fd98b1503da46dc93bfb
ruabidd <Daniil.BORODIN@raiffeisen.ru>
2025-06-16 17:27:40 +0300

    add Auth login test add login-dto.ts

--- tests/simple-solution-api.spec.ts
+++ tests/simple-solution-api.spec.ts
@@ -1,7 +1,5 @@
 import { expect, test } from '@playwright/test'
 
-import { StatusCodes } from 'http-status-codes'
-
 test('get order with correct id should receive code 200', async ({ request }) => {
   // Build and send a GET request to the server
   const response = await request.get('https://backend.tallinn-learning.ee/test-orders/1')
@@ -12,7 +10,19 @@ test('get order with correct id should receive code 200', async ({ request }) =>
   expect(response.status()).toBe(200)
 })
 
-test('post order with correct data should receive code 201', async ({ request }) => {
+test('get order with incorrect id should receive code 400', async ({ request }) => {
+  // Build and send a GET request to the server
+  const response = await request.get('https://backend.tallinn-learning.ee/test-orders/0')
+  // Log the response status, body and headers
+  const responseBody = await response.json()
+  console.log('response body:', await response.json())
+  console.log('response headers:', response.headers())
+  // Check if the response status is 200
+  expect(response.status()).toBe(400)
+  expect(responseBody.message).toBe('getById.id: must be greater than or equal to 1')
+})
+
+test('post order with correct data should receive code 200', async ({ request }) => {
   // prepare request body
   const requestBody = {
     status: 'OPEN',
@@ -29,5 +39,16 @@ test('post order with correct data should receive code 201', async ({ request })
   // Log the response status and body
   console.log('response status:', response.status())
   console.log('response body:', await response.json())
-  expect(response.status()).toBe(StatusCodes.OK)
+  expect(response.status()).toBe(200)
+})
+
+test('post order with incorrect media type should receive code 415', async ({ request }) => {
+  // Send a POST request to the server
+  const response = await request.post('https://backend.tallinn-learning.ee/test-orders', {
+    data: 'test',
+  })
+  // Log the response status and body
+  console.log('response status:', response.status())
+  console.log('response body:', await response.json())
+  expect.soft(response.status()).toBe(415)
 })
